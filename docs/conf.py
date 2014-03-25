b3a3d4d13e44bf1e1c1fe9c7da224837857178e2
Tim Lauridsen <timlau@fedoraproject.org>
2014-03-25 12:31:24 +0100

    doc: more ReadTheDocs fixes

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['gi']
+MOCK_MODULES = ['gi','gi.repository']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
