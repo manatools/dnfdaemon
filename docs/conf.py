00f9bfec6601a06b56fd8030fc04b008fe66082f
Tim Lauridsen <timlau@fedoraproject.org>
2014-03-25 12:21:21 +0100

    doc: Mock som modules, so doc will build on ReadTheDocs

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,32 @@
 # serve to show the default.
 
 import sys, os
+import sys
+
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['gi']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
