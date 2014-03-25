9219685674b1621dff78aef04e89ab7861ddf8c8
Tim Lauridsen <timlau@fedoraproject.org>
2014-03-25 15:13:07 +0100

    doc: Added some fake libs to build docs without GObject instrospection is installed

--- docs/fake-lib/gi/repository/Gio.py
+++ docs/fake-lib/gi/repository/Gio.py
@@ -1,3 +1,34 @@
+
+
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+
+BusType = Mock()
+DBusProxy = Mock()
+
+class MainLoop(Mock):
+    def __init__(self):
+        Mock.__init__(self)
+
 def bus_get_sync(*args):
     pass
     
